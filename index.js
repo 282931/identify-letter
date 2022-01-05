0287e38192ce93d8e262372e1509bdf2cf9d6ad7
xuzh <for4die@sina.com>
2022-01-05 13:53:25 +0800

    eee

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
 import App from './App';
+import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-
+console.log('test');
 AppRegistry.registerComponent(appName, () => App);
